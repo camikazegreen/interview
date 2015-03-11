c94c3074672d3c1842e450d86c20d337c0adee13
Rebecca Macaulay <macaulay@email.arizona.edu>
2015-03-11 11:19:02 -0700

    Added default logo as UA Department Name placeholder logo. Also added logic to the page template to not show the site name and slogan if the logo is checked in theme settings.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -26,11 +26,9 @@
   <header class="header" id="header_site" role="banner">
     <div class="container">
 
-      <?php if ($logo): ?>
+      <?php if ($logo): //If the logo option is on, do not display the site name and slogan ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
+      <?php elseif ($site_name || $site_slogan): ?>
         <div class="header__name-and-slogan" id="name-and-slogan">
           <?php if ($site_name): ?>
             <h1 class="header__site-name" id="site-name">
