81d4bac723e88711cd469a488a522c2adb9329b9
Rebecca <macaulay@email.arizona.edu>
2015-04-08 11:18:13 -0700

    Added footer image upload field in the Theme Settings area - file not being called/placed by the template yet.

--- theme-settings.php
+++ theme-settings.php
@@ -13,6 +13,19 @@ function ua_zen_form_system_theme_settings_alter(&$form, &$form_state, $form_id 
     return;
   }
 
+  //Add secodary logo upload field to theme settings. Code source: http://stackoverflow.com/questions/12177175/how-do-i-properly-add-file-field-in-drupal-form-via-theme-setting-php
+  $form['logo']['footer_logo_file'] = array(
+    '#type' => 'managed_file',
+    '#required' => FALSE,
+    '#title' => t('Footer logo'),
+    '#description' => t('The uploaded logo will be displayed in the site\'s footer. If no logo is specified, the main logo for the site will be used.'),
+    '#default_value' => theme_get_setting('footer_logo_file'),
+    '#upload_location' => 'public://theme/',
+    '#upload_validators' => array(
+          'file_validate_extensions' => array('gif png jpg jpeg'),
+        ),
+  );
+
   // Create the form using Forms API: http://api.drupal.org/api/7
 
   /* -- Delete this line if you want to use this setting
