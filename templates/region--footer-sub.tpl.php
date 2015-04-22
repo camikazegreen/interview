278621f3c4411b31d3451bbf3ec331d572f6edd0
Rebecca <macaulay@email.arizona.edu>
2015-04-22 15:58:49 -0700

    Added sub footer region template file and added region to page templates. Had to break the #footer_site div out into the main page template in order to nest the sub footer into the main footer area. Still need to force the sub footer region to render even if no blocks are specified (for global footer copyright)

---
+++ templates/region--footer-sub.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the sub footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+<div id="footer_sub" class="<?php print $classes; ?>">
+  	<div class="container">
+    	<?php print $content; ?>
+    	<div class="copyright">
+	    	<p>standard/global footer copyright goes here?</p>
+    	</div>
+  	</div>
+</div>
--- templates/region--footer.tpl.php
+++ templates/region--footer.tpl.php
@@ -7,15 +7,15 @@
  * @see https://drupal.org/node/1728140
  */
 ?>
-<footer id="footer_site" class="<?php print $classes; ?>">
-  	<div class="container">
-  		<div class="footer-logo">
-		  	<?php if ($ua_zen_footer_logo != "/"): ?>
-		  		<img src="<?php print $ua_zen_footer_logo; ?>" alt="" />
-		  	<?php else: ?>
-		  		<img src="<?php print $logo; ?>" alt="" />
-	    	<?php endif; ?>
-    	</div>
-    	<?php print $content; ?>
-  	</div>
-</footer>
+<div class="container">
+	<div class="footer-logo">
+		<?php if ($ua_zen_footer_logo != "/"): ?>
+			<img src="<?php print $ua_zen_footer_logo; ?>" alt="" />
+		<?php else: ?>
+			<img src="<?php print $logo; ?>" alt="" />
+		<?php endif; ?>
+	</div>
+	<div class="footer-content">
+	    <?php print $content; ?>
+	</div>
+</div>
