2c90cb4636d3d3f9d0abe5c4362e4d720dc35d16
Rebecca <macaulay@email.arizona.edu>
2015-05-01 15:00:55 -0700

    Made date piece in sub footer output via php (forgot to implement earlier)

--- templates/region--footer-sub.tpl.php
+++ templates/region--footer-sub.tpl.php
@@ -10,6 +10,6 @@
 <div id="footer_sub" class="<?php print $classes; ?>">
   	<div class="container">
     	<?php print $content; ?>
-	    <p class="copyright">Copyright © <script>new Date().getFullYear()>document.write(new Date().getFullYear());</script> Arizona Board of Regents. <a href="http://www.arizona.edu" target="_blank">The University of Arizona</a>, Tucson, Arizona</p>
+	    <p class="copyright">Copyright © <?php print date('Y'); ?> Arizona Board of Regents. <a href="http://www.arizona.edu" target="_blank">The University of Arizona</a>, Tucson, Arizona</p>
   	</div>
 </div>
