4b0a131bb62a63ed7ee4c1385c7bb3affbfafe2b
Rebecca <macaulay@email.arizona.edu>
2015-05-01 14:56:30 -0700

    Re-structured logic in footer template to only include one container element if either the logo or content are active.

--- templates/region--footer.tpl.php
+++ templates/region--footer.tpl.php
@@ -7,21 +7,21 @@
  * @see https://drupal.org/node/1728140
  */
 ?>
-<?php if ($logo): ?>
-	<div class="container">
+<?php if ($logo || $content): ?>
+<div class="container">
+	<?php if ($logo): ?>
 		<div class="footer-logo">
 			<?php if ($ua_zen_footer_logo != "/"): ?>
-				<img src="<?php print $ua_zen_footer_logo; ?>" alt="" />
+					<img src="<?php print $ua_zen_footer_logo; ?>" alt="" />
 			<?php else: ?>
-				<img src="<?php print $logo; ?>" alt="" />
+					<img src="<?php print $logo; ?>" alt="" />
 			<?php endif; ?>
 		</div>
-	</div>
-<?php endif; ?>
-<?php if ($content): ?>
-	<div class="container">
+	<?php endif; ?>
+	<?php if ($content): ?>
 		<div class="footer-content clearfix">
-		    <?php print $content; ?>
+			<?php print $content; ?>
 		</div>
-	</div>
+	<?php endif; ?>
+</div>
 <?php endif; ?>
