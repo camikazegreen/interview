9ab4686859868adaa075c9f99b849d1f8619e542
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2015-05-30 11:33:53 -0700

    added contexual links styles

--- templates/node--uaqs-featured-content.tpl.php
+++ templates/node--uaqs-featured-content.tpl.php
@@ -1,5 +1,5 @@
 <?php print render($title_prefix); ?>
-<div class="relative pull-left"><?php print render($title_suffix); ?></div>
+<span class="relative pull-left"><?php print render($title_suffix); ?></span>
 <a href="<?php print render($ua_featured_content['path']); ?>">
   <div class="slide-image-container" style="background-image: url('<?php print render($ua_featured_content['image_path']); ?>')">
       <div class="mask white">
