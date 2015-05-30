0d817b37394def731aa97cbdedd6f648e113ac8c
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2015-05-30 11:04:34 -0700

    more styles and contextual linksOC

--- templates/node--uaqs-featured-content.tpl.php
+++ templates/node--uaqs-featured-content.tpl.php
@@ -1,3 +1,4 @@
+<?php print render($title_prefix); ?>
 <a href="<?php print render($ua_featured_content['path']); ?>">
   <div class="slide-image-container" style="background-image: url('<?php print render($ua_featured_content['image_path']); ?>')">
       <div class="mask white">
@@ -6,3 +7,4 @@
       </div>
   </div>
 </a>
+<?php print render($title_suffix); ?>
