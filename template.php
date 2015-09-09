18eb68a3b4783943ed8fa2d776bd404b69bcee0e
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2015-09-08 20:59:44 -0700

    UADIGITAL-209 add bootstrap classes to menu local task

--- template.php
+++ template.php
@@ -266,3 +266,26 @@ function ua_zen_breadcrumb($variables) {
   return $output;
 }
 
+/**
+ * Overrides theme_menu_local_tasks().
+ */
+function ua_zen_menu_local_tasks(&$variables) {
+  $output = '';
+
+  if (!empty($variables['primary'])) {
+    $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $variables['primary']['#prefix'] .= '<ul class="tabs--primary nav nav-tabs">';
+    $variables['primary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables['primary']);
+  }
+
+  if (!empty($variables['secondary'])) {
+    $variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+    $variables['secondary']['#prefix'] .= '<ul class="tabs--secondary pagination pagination-sm">';
+    $variables['secondary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables['secondary']);
+  }
+
+  return $output;
+}
+
