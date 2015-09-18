8143f9e1bebaa67adf02b6cb486f6cd4dd18de83
Alex Wochna <awochna@email.arizona.edu>
2015-09-18 10:11:46 -0700

    Add in the new 4th news item to the test. Everything should pass now.

--- suites/ua_news.js
+++ suites/ua_news.js
@@ -13,20 +13,25 @@ casper.test.begin('News dates sort properly in the news view', function suite(te
       });
     });
 
-    var expected = ['Tuesday, July 21, 2015',
+    var expected = ['Wednesday, September 9, 2015',
+                    'Tuesday, July 21, 2015',
                     'Monday, May 4, 2015',
                     'Saturday, March 14, 2015',];
 
     test.assertEquals(dates[0], expected[0]);
     test.assertEquals(dates[1], expected[1]);
     test.assertEquals(dates[2], expected[2]);
+    test.assertEquals(dates[3], expected[3]);
 
     test.assertNotEquals(dates[0], expected[1]);
     test.assertNotEquals(dates[0], expected[2]);
+    test.assertNotEquals(dates[0], expected[3]);
     test.assertNotEquals(dates[1], expected[0]);
     test.assertNotEquals(dates[1], expected[2]);
+    test.assertNotEquals(dates[1], expected[3]);
     test.assertNotEquals(dates[2], expected[0]);
     test.assertNotEquals(dates[2], expected[1]);
+    test.assertNotEquals(dates[2], expected[3]);
   });
 
   casper.run(function() {
